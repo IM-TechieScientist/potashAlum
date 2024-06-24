ce760166f84c52345516dcfb28de8912e0cb0c80
ramanan-br <146334788+ramanan-br@users.noreply.github.com>
2024-06-24 10:51:43 +0530

    Added page 2 and implemented data transfer between pages

--- lib/pg2.dart
+++ lib/pg2.dart
@@ -1,17 +1,50 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
-class NewPage extends StatelessWidget {
+class NewPage extends StatefulWidget {
+  final String colgname;
+  NewPage(this.colgname);
+
+  @override
+  State<NewPage> createState() => _NewPageState(this.colgname);
+}
+
+class _NewPageState extends State<NewPage> {
+  String colgname;
+  _NewPageState(this.colgname);
   @override
   Widget build(BuildContext context) {
+    var deftextstyle = TextStyle(fontSize: 30);
+    var namelist = ['kaqwla', 'amala', 'kalaa'];
     return Scaffold(
-      appBar: AppBar(
-        title: Text('New Page'),
-      ),
-      body: Center(
-        child: Text('This is a new page'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text(
+            'maala',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Color.fromARGB(255, 234, 85, 209), fontSize: 30),
+          ),
+          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+          centerTitle: true,
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+                color: Colors.black, style: BorderStyle.solid, width: 1.0),
+          ),
+          child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: EdgeInsets.fromLTRB(500, 10, 500, 10),
+                  child: ListTile(
+                    hoverColor: Colors.amber,
+                    titleAlignment: ListTileTitleAlignment.center,
+                    title: Text(namelist[index]),
+                    subtitle: Text(colgname),
+                    trailing: Text('2020-24'),
+                  ),
+                );
+              }),
+        ));
   }
 }
